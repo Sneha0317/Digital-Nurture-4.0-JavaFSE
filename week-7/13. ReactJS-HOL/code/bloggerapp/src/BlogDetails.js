d56c058a9caaf7b19ac08034936ef587c36cbc66
Sneha0317 <snehapapineni1@gmail.com>
2025-08-03 19:45:26 +0530

    week-7

--- week-7/13. ReactJS-HOL/code/bloggerapp/src/BlogDetails.js
+++ week-7/13. ReactJS-HOL/code/bloggerapp/src/BlogDetails.js
@@ -3,9 +3,8 @@ import React from 'react';
 const BlogDetails = ({ blogs }) => {
   return (
     <div>
-      <h2>Blog Details</h2>
-      {blogs.map((blog, index) => (
-        <div key={index}>
+      {blogs.map(blog => (
+        <div key={blog.id}>
           <h3>{blog.title}</h3>
           <h4>{blog.author}</h4>
           <p>{blog.content}</p>
