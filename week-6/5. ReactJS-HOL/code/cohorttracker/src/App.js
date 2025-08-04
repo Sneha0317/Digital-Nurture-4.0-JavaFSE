1fc9bce4327752bfe96bbd5e899264256a7870e8
Sneha0317 <snehapapineni1@gmail.com>
2025-08-04 20:56:32 +0530

    week-6

--- week-6/5. ReactJS-HOL/code/cohorttracker/src/App.js
+++ week-6/5. ReactJS-HOL/code/cohorttracker/src/App.js
@@ -1,36 +1,15 @@
-import React from "react";
-import CohortDetails from "./CohortDetails";
-import styles from "./CohortDetails.module.css";
+import logo from './logo.svg';
+import './App.css';
+import { CohortsData} from './Cohort'
+import CohortDetails from './CohortDetails'; 
 
 function App() {
+ 
   return (
-    <div>
-      <p className={styles.title}>Cohorts Details</p>
-
-      <CohortDetails
-        id="INTADMDF10 - .NET FSD"
-        startDate="22-Feb-2022"
-        status="Scheduled"
-        coach="Aathma"
-        trainer="Jojo Jose"
-      />
-
-      <CohortDetails
-        id="ADM21JF014 - Java FSD"
-        startDate="10-Sep-2021"
-        status="Ongoing"
-        coach="Apoorv"
-        trainer="Elisa Smith"
-      />
-
-      <CohortDetails
-        id="CDBJF21025 - Java FSD"
-        startDate="24-Dec-2021"
-        status="Ongoing"
-        coach="Aathma"
-        trainer="John Doe"
-      />
-    </div>
+  <div>
+    <h1>Cohorts Details</h1>
+    {CohortsData.map(cohort => <CohortDetails cohort={cohort}/>)}
+  </div>
   );
 }
 
--- week-6/5. ReactJS-HOL/code/cohorttracker/src/CohortDetails.js
+++ week-6/5. ReactJS-HOL/code/cohorttracker/src/CohortDetails.js
@@ -1,28 +1,29 @@
-import React from "react";
-import styles from "./CohortDetails.module.css";
+import styles from './CohortDetails.module.css';
+function CohortDetails(props) {
+    const Status = props.cohort.currentStatus.toLowerCase();
 
-function CohortDetails({ id, startDate, status, coach, trainer }) {
-  const statusClass = status === "Ongoing" ? styles.ongoing : styles.nonOngoing;
+    const heading_colour={
+        color: Status ==='ongoing' ? 'green' : 'blue'
+    }
+    return (
+        <div className={styles.box}>
+            <h3 style={heading_colour}>
+                {props.cohort.cohortCode} -
+                <span>{props.cohort.technology}</span>
+            </h3>
+            <dl>
+                <dt>Started On</dt>
+                <dd>{props.cohort.startDate}</dd>
+                <dt>Current Status</dt>
+                <dd>{props.cohort.currentStatus}</dd>
+                <dt>Coach</dt>
+                <dd>{props.cohort.coachName}</dd>
+                <dt>Trainer</dt>
+                <dd>{props.cohort.trainerName}</dd>
+            </dl>
+        </div>
 
-  return (
-    <div className={styles.box}>
-      <h3 className={statusClass}>{id}</h3>
-      <dl>
-        <dt>Started On</dt>
-        <dd>{startDate}</dd>
-
-        <dt>Current Status</dt>
-        <dd>{status}</dd>
-
-        <dt>Coach</dt>
-        <dd>{coach}</dd>
-
-        <dt>Trainer</dt>
-        <dd>{trainer}</dd>
-      </dl>
-    </div>
-  );
+        
+    );
 }
-
 export default CohortDetails;
-
